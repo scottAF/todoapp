edfd5381779776eb15fa3a7e83a7ba77e1d546cb
Scott Ford <uofoduck14@gmail.com>
2019-08-21 18:23:48 +0000

    added the ability to create new tasks for the todo list

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -8,17 +8,21 @@ class TasksController < ApplicationController
     task.new
   end
 
-
   def update    
     task = Task.find(params[:id])
     task.update_attributes(task_params)
     render json: task
   end
 
+  def create
+    task = Task.create(task_params)
+    render json: task
+  end
+
   private
 
   def task_params
-    params.require(:task).permit(:done)
+    params.require(:task).permit(:done, :title)
   end
 
 
